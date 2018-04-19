486df59e8c0752bc921cd9786f8adce8c9fbf4a0
Chiel Kunkels <chiel@kunkels.me>
2018-04-20 00:22:43 +0200

    core: add not found + error handlers

--- pkg/core/src/server/OmniServer.js
+++ pkg/core/src/server/OmniServer.js
@@ -1,5 +1,8 @@
 import express from 'express';
 
+import handleErrorMiddleware from './utils/handleErrorMiddleware';
+import notFoundMiddleware from './utils/notFoundMiddleware';
+
 export default class OmniServer {
 	constructor() {
 		this.app = express();
@@ -7,6 +10,9 @@ export default class OmniServer {
 	}
 
 	start() {
+		this.app.use(notFoundMiddleware);
+		this.app.use(handleErrorMiddleware);
+
 		this.app.listen(12831, () => {
 			console.info('Omni app listening on port 12831');
 		});
