a25df967293f1a2c2acdd3f03f00ebfbc0a4d60e
Chiel Kunkels <chiel@kunkels.me>
2018-04-20 01:07:23 +0200

    core: expose plugin api

--- pkg/core/src/server/OmniServer.js
+++ pkg/core/src/server/OmniServer.js
@@ -4,17 +4,32 @@ import handleErrorMiddleware from './utils/handleErrorMiddleware';
 import notFoundMiddleware from './utils/notFoundMiddleware';
 
 export default class OmniServer {
+	pluginApi = {};
+	plugins = [];
+
 	constructor() {
 		this.app = express();
 		this.app.disable('x-powered-by');
 	}
 
 	start() {
+		this.plugins.forEach(plugin => {
+			plugin(this.pluginApi);
+		});
+
 		this.app.use(notFoundMiddleware);
 		this.app.use(handleErrorMiddleware);
 
 		this.app.listen(12831, () => {
 			console.info('Omni app listening on port 12831');
 		});
+	}
+
+	use(plugin) {
+		if (typeof plugin !== 'function') {
+			throw new Error('Omni plugins need to be functions');
+		}
+
+		this.plugins.push(plugin);
 	}
 }
