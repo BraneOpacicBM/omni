3b0ebe4de16991b8ed474052a39b9dcf69211977
Chiel Kunkels <chiel@kunkels.me>
2018-04-20 00:54:37 +0200

    core: render markup into a full html document

---
+++ pkg/core/src/server/utils/renderDocument.js
@@ -0,0 +1,22 @@
+import { renderToString } from 'react-dom/server';
+
+export default function renderDocument(component) {
+	const markup = renderToString(component);
+
+	/* eslint-disable indent */
+	return (
+`<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Omni</title>
+	</head>
+	<body>
+		<div id="app-container">${markup}</div>
+	</body>
+</html>`
+	);
+	/* eslint-enable indent */
+}
