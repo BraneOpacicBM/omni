8f6e674c953574582686592df2fdc9acb2aeebcb
Chiel Kunkels <chiel@kunkels.me>
2018-05-03 22:54:10 +0200

    core: add form hoc to build forms

--- pkg/core/src/client/index.js
+++ pkg/core/src/client/index.js
@@ -1,2 +1,3 @@
 export Omni from './OmniClient';
 export Button from '../app/components/Button';
+export withForm from '../app/components/Form';
--- pkg/core/src/server/index.js
+++ pkg/core/src/server/index.js
@@ -1,2 +1,3 @@
 export Omni from './OmniServer';
 export Button from '../app/components/Button';
+export withForm from '../app/components/Form';
