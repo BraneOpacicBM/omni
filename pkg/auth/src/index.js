d90c63abb17b91304729f0b737b4f3fba77cf2b1
Chiel Kunkels <chiel@kunkels.me>
2018-05-08 23:15:56 +0200

    auth: add method to create jwt hashes

--- pkg/auth/src/index.js
+++ pkg/auth/src/index.js
@@ -1,6 +1,9 @@
+import jwt from 'jsonwebtoken';
 import React from 'react';
 
 import Login from './components/Login';
+
+const salt = process.env.AUTH_TOKEN_SALT;
 
 export default function authPlugin(omni) {
 	const providers = [];
@@ -14,5 +17,14 @@ export default function authPlugin(omni) {
 		providers.push({ Component, name });
 	};
 
-	omni.auth = { addProvider };
+	const createToken = (type, subject, secret) => (
+		new Promise((resolve, reject) => {
+			jwt.sign({ type }, salt + secret, { subject: `${subject}` }, (err, token) => {
+				if (err) return reject(err);
+				resolve(token);
+			});
+		})
+	);
+
+	omni.auth = { addProvider, createToken };
 }
