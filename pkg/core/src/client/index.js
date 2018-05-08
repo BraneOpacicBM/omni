2c0e284afd3afc2fa5383467b9d1c8ff968a207b
Chiel Kunkels <chiel@kunkels.me>
2018-05-08 22:22:20 +0200

    core: add createAction method

--- pkg/core/src/client/index.js
+++ pkg/core/src/client/index.js
@@ -2,3 +2,4 @@ export Omni from './OmniClient';
 export Button from '../app/components/Button';
 export withForm from '../app/components/Form';
 export classNames from '../app/utils/classNames';
+export createAction from '../app/utils/createAction';
