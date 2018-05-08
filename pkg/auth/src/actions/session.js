73f5a8ff467af84f9c517a5e77a4dcd3c98481fe
Chiel Kunkels <chiel@kunkels.me>
2018-05-09 00:54:20 +0200

    auth: implement setToken/setUser actions

---
+++ pkg/auth/src/actions/session.js
@@ -0,0 +1,4 @@
+import { createAction } from '@ocm/core';
+
+export const setToken = createAction('session/set_token');
+export const setUser = createAction('session/set_user');
--- pkg/auth/src/reducers/session.js
+++ pkg/auth/src/reducers/session.js
@@ -1,8 +1,24 @@
+import { setToken, setUser } from '../actions/session';
+
 export const initialState = {
 	token: '',
 	user: null,
 };
 
-export default function sessionReducer(state = initialState) {
+export default function sessionReducer(state = initialState, action) {
+	if (action.type === setToken.type) {
+		return {
+			...state,
+			token: action.token,
+		};
+	}
+
+	if (action.type === setUser.type) {
+		return {
+			...state,
+			user: action.user,
+		};
+	}
+
 	return state;
 }
