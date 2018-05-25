c664ed7fb2f16dfcc7b860ea55c1084b73122378
Chiel Kunkels <chiel@kunkels.me>
2018-05-25 11:29:05 +0200

    auth: allow for setting a custom login screen layout

--- pkg/auth/src/init.js
+++ pkg/auth/src/init.js
@@ -5,15 +5,21 @@ import providers from './providers';
 import sessionReducer from './reducers/session';
 
 export default function authPlugin(omni) {
+	let Layout = Login;
+
 	omni.addRoute({
 		path: '/login',
-		component: () => <Login providers={providers} />,
+		component: () => React.createElement(Layout, { providers }),
 	});
 
 	const addProvider = (name, Component, getSecret) => {
 		providers.push({ Component, getSecret, name });
 	};
 
-	omni.auth = { addProvider };
+	const setLayout = layout => {
+		Layout = layout;
+	};
+
+	omni.auth = { addProvider, setLayout };
 	omni.reducers.session = sessionReducer;
 }
